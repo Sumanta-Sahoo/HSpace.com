79c6625f2680533491c022fd74c4437df211ae3f
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-05 19:38:09 +0530

    Setting up project folder for backend development

---
+++ src/main/java/com/hSpace/HSpace/HSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.hSpace.HSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hSpace/HSpace/HSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hSpace.HSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
