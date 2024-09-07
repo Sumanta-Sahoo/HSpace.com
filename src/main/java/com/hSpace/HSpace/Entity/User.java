88a3d2de2b4a1c4fa8cd8c5344a1d23b91327c22
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-07 08:31:05 +0530

    Modifying association to bookingList

--- src/main/java/com/hSpace/HSpace/Entity/User.java
+++ src/main/java/com/hSpace/HSpace/Entity/User.java
@@ -31,6 +31,7 @@ public class User implements UserDetails {
     @NotBlank(message = "Passwords is required")
     private String password;
     private String role;
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookings = new ArrayList<>();
 
 
