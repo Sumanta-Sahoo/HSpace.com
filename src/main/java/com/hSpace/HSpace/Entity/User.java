b5eea6e6b24576ab1e2e5fa1e409bd6fb4d870ce
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-13 20:19:19 +0530

    fix(user): Add @NoArgsConstructor to User entity to resolve instantiation error

--- src/main/java/com/hSpace/HSpace/Entity/User.java
+++ src/main/java/com/hSpace/HSpace/Entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +16,7 @@ import java.util.List;
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User implements UserDetails {
 
