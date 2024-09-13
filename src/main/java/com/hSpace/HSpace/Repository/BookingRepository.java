5a457b544159ef910cfdfdac9e5d39efd3502787
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-13 20:11:54 +0530

    feat(booking): Add repository methods for querying bookings by room, user, and confirmation code
    - Implemented method to retrieve bookings by room ID for room-based booking management
    - Added method to fetch a booking by confirmation code for efficient booking lookup
    - Developed method to find bookings by user ID for personalized user booking history

--- src/main/java/com/hSpace/HSpace/Repository/BookingRepository.java
+++ src/main/java/com/hSpace/HSpace/Repository/BookingRepository.java
@@ -4,10 +4,11 @@ import com.hSpace.HSpace.Entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByRoomId(Long roomId);
-    List<Booking> findByBookingConfirmationCode(String bookingConfirmationCode);
+    Optional<Booking> findByBookingConfirmationCode(String bookingConfirmationCode);
     List<Booking> findByUserId(Long userId);
 }
