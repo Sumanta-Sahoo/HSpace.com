6ee8ca761abd366ef5ba733ccb7c781a61c89d0f
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-13 20:21:55 +0530

    fix(Booking): Add @NoArgsConstructor to Booking entity to resolve instantiation error

--- src/main/java/com/hSpace/HSpace/Entity/Booking.java
+++ src/main/java/com/hSpace/HSpace/Entity/Booking.java
@@ -6,12 +6,14 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "bookings")
 public class Booking {
 
